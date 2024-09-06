4ae565aa0a9a8ae527dbc02d8740e8e0d7bde64b
Leo Flapper <dominicus1991@gmail.com>
2024-09-06 11:40:31 +0200

    Added 'ContactInformation' to 'Invoicer'

--- src/Parts/Invoicer.php
+++ src/Parts/Invoicer.php
@@ -6,11 +6,15 @@ use Wefabric\GS1InsbouOrderConverter\Validatable;
 
 class Invoicer extends BaseAddressParty implements Validatable
 {
+    public ?ContactInformationList $ContactInformation;
 
     public function __construct(array $data = [])
     {
+        if(isset($data['ContactInformation']) && is_array($data['ContactInformation'])){
+            $data['ContactInformation'] = new ContactInformationList($data['ContactInformation']);
+        }
+        
         parent::__construct($data);
         $this->PartyType = PartyType::Invoicer;
     }
-
 }
